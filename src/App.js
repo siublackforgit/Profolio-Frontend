2a8511c488f8ecad561288e11041fcb75f0ab958
Yeung Ka Chun <siublackbusiness@gmail.com>
2025-11-07 21:20:29 +0800

    test Jenkin

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Check <code>src/App.js</code> and save to reload.
+          Check Jenkins <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
